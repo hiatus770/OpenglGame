0260c6da8eb5920d0f653400e1b02e3fc85194fc
Hiatus <hiatus.cool@gmail.com>
2024-03-14 18:52:41 -0400

    Created Repository With Examples from LearnOPENGL

---
+++ myProject/build/CMakeFiles/GraphicsStuff.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphicsStuff.dir/src/main.cpp.o"
+  "CMakeFiles/GraphicsStuff.dir/src/main.cpp.o.d"
+  "GraphicsStuff"
+  "GraphicsStuff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphicsStuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
