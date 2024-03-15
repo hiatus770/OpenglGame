f9647f76b9f80a9aa2b137723e1f5b4f800b5f00
Hiatus <hiatus.cool@gmail.com>
2024-03-14 23:02:33 -0400

    It now draws a triangle

--- myProject/src/main.cpp
+++ myProject/src/main.cpp
@@ -2,16 +2,28 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-// This function is going to be used to resize the viewport everytime it is resized by the user 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}  
+const int SRC_WIDTH = 800;
+const int SRC_HEIGHT = 600;
 
-void processInput(GLFWwindow *window){
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true); 
-}
+// Whenever the window is changed this function is called
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+// Processing all input here
+void processInput(GLFWwindow *window);
+
+// Defining our vertices
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f};
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\nout vec4 FragColor;\nvoid main()\n{FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}"; 
 
 int main()
 {
@@ -21,10 +33,15 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // The minor version of GLFW we are using so x.x the second x
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // This means we do not use any backwards compatible features, so its a smaller set of all of OPENGL
 
-    // Creating the window object
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Window!", NULL, NULL);
+// Need this to run on apple devices
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    // IF it wasn't created then we stop here
+    // Creating the window object
+    GLFWwindow *window = glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "Window!", NULL, NULL);
+
+    // If it wasn't created then we stop here
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW Window" << std::endl;
@@ -33,29 +50,100 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
-    // Load GLAD function pointers so that we use the correct openGL functions 
+    // Load GLAD function pointers so that we use the correct openGL functions
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // Viewport
-    glViewport(0,0, 800, 600); 
+    // Set the frameBufferSizeballback function
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Set the frameBufferSizeballback function 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
-    // Main Loop of the function
-    while(!glfwWindowShouldClose(window)){
-        // Process input call
-        processInput(window); 
+    // SHADER STUFF
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
-        glfwSwapBuffers(window); // Swaps the color buffer that is used to render to during this render iteration and show it ot the output screen 
-        glfwPollEvents(); // Checks if any events are triggered, updates the window state andcalls the corresponding functions 
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
 
+    // Fragment shader stuff
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
 
-    glfwTerminate(); // Call this function to properly clean up GLFW's big mess 
+    // Shader program
+    unsigned int shaderProgram; 
+    shaderProgram = glCreateProgram(); 
+
+    glAttachShader(shaderProgram, vertexShader); 
+    glAttachShader(shaderProgram, fragmentShader); 
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram); 
+
+    glDeleteShader(vertexShader); 
+    glDeleteShader(fragmentShader); 
+
+    // VBO STUFF
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);                                                     // Create the buffer for the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);                                        // We bind the buffer to the array target
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Write the buffer data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+    glEnableVertexAttribArray(0);  
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);  
+    glBindVertexArray(VAO); 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+    glEnableVertexAttribArray(0); 
+
+    // Main Loop of the function
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear the screen before we start
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Process input call
+        processInput(window);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0,3); 
+
+
+        glfwSwapBuffers(window); // Swaps the color buffer that is used to render to during this render iteration and show it ot the output screen
+        glfwPollEvents();        // Checks if any events are triggered, updates the window state andcalls the corresponding functions
+    }
+
+    glfwTerminate(); // Call this function to properly clean up GLFW's big mess
     return 0;
+}
+
+// This function is going to be used to resize the viewport everytime it is resized by the user
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
